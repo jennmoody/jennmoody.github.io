11cee07fa3c645eddab65028147513fef22fb638
jennmoody <moo18032@byui.edu>
2019-04-12 22:15:37 -0600

    enter

--- assignments/final/scripts/temple.js
+++ assignments/final/scripts/temple.js
@@ -14,6 +14,7 @@ templeRequest.onload = function () {
 
     var closureUL = document.createElement("ul");
     var closureLI;
+   
 
     for (var i = 0; i < templeData[0].closures.length; i++) {
         closureLI = document.createElement("li");
@@ -21,27 +22,13 @@ templeRequest.onload = function () {
             ' to ' + templeData[0].closures[i].end;
         closureUL.appendChild(closureLI);
 
-               
-        var templeClosures;
-        if (i === 0) {
-            templeClosures = 'templeClosures1';
-        } 
-        else if (i === 1) {
-            templeClosures = 'templeClosures2';
-        }
-        else if (i === 2) {
-            templeClosures = 'templeClosures3';
-        }
-        else if (i === 3) {
-            templeClosures = 'templeClosures4';
-        }
+    }       
+        document.getElementById('templeClosures1').appendChild(closureUL);
+      
 
-
-        document.getElementById(templeClosures).appendChild(closureUL);
-    
-    }
-    
 }
+
+
 
 
 
